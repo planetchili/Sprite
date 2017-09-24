f7d990f034a93e00a137775a1dd04588352d486d
ahmadsarvmeily <as20g16@ecs.soton.ac.uk>
2017-09-25 00:01:23 +0100

    Added handling of self-assignment for Sprite class

--- Engine/Surface.cpp
+++ Engine/Surface.cpp
@@ -62,16 +62,18 @@ Surface::~Surface()
 
 Surface& Surface::operator=( const Surface& rhs )
 {
-	width = rhs.width;
-	height = rhs.height;
+	if (&rhs != this) {
+		width = rhs.width;
+		height = rhs.height;
 
-	delete [] pPixels;
-	pPixels = new Color[width*height];
+		delete[] pPixels;
+		pPixels = new Color[width*height];
 
-	const int nPixels = width * height;
-	for( int i = 0; i < nPixels; i++ )
-	{
-		pPixels[i] = rhs.pPixels[i];
+		const int nPixels = width * height;
+		for (int i = 0; i < nPixels; i++)
+		{
+			pPixels[i] = rhs.pPixels[i];
+		}
 	}
 	return *this;
 }
