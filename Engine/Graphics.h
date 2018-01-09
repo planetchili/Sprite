ba8a1dcc31ccf9340bf3ecc4f2a370ff21db0824
planetchili <planetchili@users.noreply.github.com>
2018-01-09 22:29:33 +0900

    add optimized versions for Chroma and Substitution sprite effects

--- Engine/Graphics.h
+++ Engine/Graphics.h
@@ -136,4 +136,8 @@ public:
 #ifndef GOD_GRAPHICS
 extern template
 void Graphics::DrawSprite<SpriteEffect::Copy>( int x,int y,RectI srcRect,const RectI& clip,const Surface& s,SpriteEffect::Copy effect );
+extern template
+void Graphics::DrawSprite<SpriteEffect::Chroma>( int x,int y,RectI srcRect,const RectI& clip,const Surface& s,SpriteEffect::Chroma effect );
+extern template
+void Graphics::DrawSprite<SpriteEffect::Substitution>( int x,int y,RectI srcRect,const RectI& clip,const Surface& s,SpriteEffect::Substitution effect );
 #endif
--- Engine/GraphicsOD.cpp
+++ Engine/GraphicsOD.cpp
@@ -28,4 +28,6 @@ Color Graphics::GetPixel( int x,int y ) const
 void dummy( Graphics& gfx )
 {
 	gfx.DrawSprite( 0,0,RectI{ 0,0,0,0 },RectI{ 0,0,0,0 },Surface{},SpriteEffect::Copy{} );
+	gfx.DrawSprite( 0,0,RectI{ 0,0,0,0 },RectI{ 0,0,0,0 },Surface{},SpriteEffect::Chroma{ Colors::Black } );
+	gfx.DrawSprite( 0,0,RectI{ 0,0,0,0 },RectI{ 0,0,0,0 },Surface{},SpriteEffect::Substitution{ Colors::Black,Colors::Black } );
 }
